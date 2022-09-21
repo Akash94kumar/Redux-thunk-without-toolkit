ff60843a376048c50836639d3ab412f4db45e63a
DESKTOP-CP98QQM\aakas <aakash94soni@gmail.com>
2022-09-21 13:40:53 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+import "./App.css";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userAction } from "./redux/actions/userActions";
+import User from "./components/User";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.userList);
+  const { loading, users, error } = userList;
+  useEffect(() => {
+    dispatch(userAction);
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redux thunk tutiro</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <User user={users} />
+      )}
     </div>
   );
 }
